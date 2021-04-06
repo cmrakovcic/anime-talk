9b11cb0b763b9962ceacb61450f96e48ca5f7c5a
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-06 13:08:04 -0400

    create action in users_controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to @user 
+        else
+            redirect_to "/login"
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
