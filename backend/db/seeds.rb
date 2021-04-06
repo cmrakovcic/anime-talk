41160d7e843d746b1b8c867bb439a374650b690a
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-06 11:42:05 -0400

    added additional seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,9 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(username: "abc123", email: "abc123@aol.com", password: "hello")
+user = User.create(username: "max902", email: "max@gmail.com", password: "maximillion")
 
 animes = Anime.create(title: "Naruto")
 animes = Anime.create(title: "One Piece")
 animes = Anime.create(title: "Dragon Ball Z")
 
 comment = Comment.create(user_id: 1, anime_id: 1, content: "This is the first anime I have watched")
+comment = Comment.create(user_id: 2, anime_id: 2, content: "This is my favorite anime!")
