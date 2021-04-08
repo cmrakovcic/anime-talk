627ba5d4af600df5794e3ebc55d899c4e87c5249
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:19:08 -0400

    index action in comments_controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+
+    def index
+        if params[:anime_id] && @anime.find_by_id(params[:anime_id])
+            @comments = @anime.comments
+        else
+            @error = "That anime doesn't exist" if params[:anime_id]
+            @comments = Comment.all
+        end
+    end
 end
