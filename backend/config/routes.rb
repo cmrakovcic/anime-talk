97ea4dd1df04c6fb3c48a5573a3692d3aa42a57e
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 14:14:32 -0400

    added route for logout

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
+  delete '/logout' => 'sessions#destroy'
+
   resources :animes do
     resources :comments
   end
