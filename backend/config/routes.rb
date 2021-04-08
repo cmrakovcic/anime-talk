c2309b561e19399e9476399d7a97cc7a0f751344
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 14:23:09 -0400

    started adding resources in routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  resources :users
+  resources :users do
+    resources :animes, shallow: true
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
