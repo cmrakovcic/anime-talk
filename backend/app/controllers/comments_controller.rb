2c455b0ac984757e09d6e98c90207a44d302bc80
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:26:15 -0400

    new action in comments_controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -8,4 +8,13 @@ class CommentsController < ApplicationController
             @comments = Comment.all
         end
     end
+
+    def new
+        if params[:anime_id] && @anime = Anime.find_by_id(params[:anime_id])
+            @comment = @anime.comments.build
+        else
+            @error = "That anime doesn't exist" if params[:anime_id]
+            @comment = Comment.new
+        end
+    end
 end
