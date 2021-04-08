a2f408886f8103a700a1921c68e21db521480fcb
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:43:04 -0400

    added helpers in the comments_controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -46,4 +46,22 @@ class CommentsController < ApplicationController
         @comment.destroy
         redirect_to comments_path
     end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :anime_id)
+    end
+
+    def set_comment
+        @comment = Comment.find_by(id: params[:id])
+        if !@comment
+            flash[:message] = "Comment was not found"
+            redirect_to comments_path
+        end
+    end
+
+    def redirect_if_not_comment_author
+        redirect_to comments_path if @comment.user != current_user
+    end
 end
