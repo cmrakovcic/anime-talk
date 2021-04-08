3142b530f59b961dfb37bd4633498596610937d9
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:31:33 -0400

    create action in comments_controller

--- backend/app/controllers/comments_controller.rb
+++ backend/app/controllers/comments_controller.rb
@@ -17,4 +17,13 @@ class CommentsController < ApplicationController
             @comment = Comment.new
         end
     end
+
+    def create
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save
+            redirect_to comments_path
+        else
+            render :json
+        end
+    end
 end
