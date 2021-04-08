837162ebc71cf6e996403bfbe7c774b65519ff04
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:46:13 -0400

    started animes_controller

--- backend/app/controllers/animes_controller.rb
+++ backend/app/controllers/animes_controller.rb
@@ -1,2 +1,13 @@
 class AnimesController < ApplicationController
+
+    def index
+        @animes = Anime.all.includes(:comments)
+    end
+
+    def show
+        @anime = Anime.find_by_id(params[:id])
+    end
+
+    def new
+    end
 end
