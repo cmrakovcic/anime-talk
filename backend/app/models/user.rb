e3e14917621c66f070e38ab580c7a6b079917b2a
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 13:59:45 -0400

    set up validations and associations in the user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :comments
+    has_many :commented_animes, through: :comments, source: :anime
+
+    validates :username, :email, presence: true
+    validates :username, uniqueness: {scope: :username, message: "already exists"}
 end
