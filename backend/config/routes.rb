a9df52ae1b4cd5ea43d6a6040a8fc227ad03a62d
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-08 14:10:44 -0400

    added route for homepage

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :animes
+  root "sessions#home"
+
+  resources :animes do
+    resources :comments
+  end
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
