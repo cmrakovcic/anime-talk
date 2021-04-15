27525829c0b3b18409df5cfe9c2d675191123189
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-15 17:57:28 -0400

    added animeadapter js

---
+++ frontend/src/adapters/AnimesAdapter.js
@@ -0,0 +1,27 @@
+class AnimesAdapter{
+    constructor() {
+      this.baseUrl = 'http://localhost:3000/animes'
+    }
+  
+    getAnimes() {
+      return fetch(this.baseUrl).then(res => res.json())
+    }
+  
+    createAnime(titleValue, creatorValue) {
+      const anime = {
+        title: titleValue,
+        creator: creatorValue,
+  
+      };
+      return fetch(this.baseUrl, {
+        method:'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body:JSON.stringify(anime)
+      })
+      .then(res => (res.json()))
+      .catch(error => console.log("Error: " + error))
+    }
+}
