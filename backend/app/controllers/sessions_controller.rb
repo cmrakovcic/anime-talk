81b00b25c5c276d740f0a8729713b5767fcbbd92
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-04-16 13:56:56 -0400

    added some logic to sessions and users controller

--- backend/app/controllers/sessions_controller.rb
+++ backend/app/controllers/sessions_controller.rb
@@ -1,16 +1,17 @@
-class SessionsController < ApplicationController
+class UsersController < ApplicationController
+
     def create
-        user = User.find_by(username: params[:user][:username])
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            render json: @user, status: 200 
-      else
-        render json: 'user login failed'
-      end
+        user = User.find_by(email: user_params[:email])
+        if user && user.authenticate(user_params[:password])
+            render json: @user
+        else
+            render json: "User login failed, please try again"
+        end
     end
-  
+
     private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+
+        def user_params
+            params.require(:user).permit(:email, :password)
+        end
 end
--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: @user, status: 200 
-        end
-    end
 
-    private
-        def user_params
-            params.require(:user).permit(:username, :email, :password)
-        end
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: @user
+    end
+  end
+
+  private
+
+    def user_params
+      params.requre(:user).permit(:username, :email, :password)
+    end
 end
